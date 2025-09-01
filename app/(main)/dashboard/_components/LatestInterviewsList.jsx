c63973b7ff5b7bc9cbb9461ad9c60553554f0549
mantu01714 <mk642799gmail.com>
2025-09-02 02:37:41 +0530

    prev dilaog dix

--- app/(main)/dashboard/_components/LatestInterviewsList.jsx
+++ app/(main)/dashboard/_components/LatestInterviewsList.jsx
@@ -9,42 +9,42 @@ import { toast } from "sonner";
 
 function LatestInterviewsList() {
   const [interviewList, setInterviewList] = useState([]);
-  const {user}=useUser();
-  useEffect(()=>{
-    user&&GetInterviewList();
-  },[user])
+  const { user } = useUser();
+  useEffect(() => {
+    user && GetInterviewList();
+  }, [user]);
 
   const GetInterviewList = async () => {
     let { data: Interviews, error } = await supabase
       .from("Interviews")
       .select("*")
-      .eq('userEmail',user?.email)
-      .order('id',{ascending:false})
-      .limit(6)
+      .eq("userEmail", user?.email)
+      .order("id", { ascending: false })
+      .limit(6);
     // console.log(Interviews)
     setInterviewList(Interviews);
-  }
+  };
 
   return (
     <div className="my-5">
       <h2 className="font-bold text-2xl">Previously Created Interviews</h2>
 
-      {interviewList?.length == 0 && 
+      {interviewList?.length == 0 && (
         <div className="p-5 flex flex-col gap-3 items-center mt-5">
           <Video className="h-10 w-10 text-primary" />
           <h2>You don't have any interview created!</h2>
           <Button>Create New Interview</Button>
         </div>
-      }
-      {interviewList&&
-        <div className="grid grid-cols-2 mt-5 xl:grid-cols-3 gap-5">
-          {interviewList.map((interview,index)=>(
-            <InterviewCard interview={interview} key={index}/>
+      )}
+      {interviewList && (
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-5 gap-5">
+          {interviewList.map((interview, index) => (
+            <InterviewCard interview={interview} key={index} />
           ))}
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 export default LatestInterviewsList;
