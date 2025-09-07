97250a04db75ced72555277596cf696c8d96ff7b
mantu01714 <mk642799gmail.com>
2025-09-07 22:38:13 +0530

    Fix user credits update 1

--- app/(main)/dashboard/_components/LatestInterviewsList.jsx
+++ app/(main)/dashboard/_components/LatestInterviewsList.jsx
@@ -2,28 +2,32 @@
 import { useUser } from "@/app/provider";
 import { Button } from "@/components/ui/button";
 import { supabase } from "@/services/supabaseClient";
-import { Camera, Video } from "lucide-react";
+import { Camera, Plus, Video } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import InterviewCard from "./InterviewCard";
 import { toast } from "sonner";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import PageLoader from "@/services/PageLoader";
 
 function LatestInterviewsList() {
   const [interviewList, setInterviewList] = useState([]);
   const { user } = useUser();
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     user && GetInterviewList();
   }, [user]);
 
   const handleInterview = () => {
-        if(!user?.credits || user?.credits < 1){
-            toast.error("You don't have enough credits! Purchase our plan to create interviews.");
-            router.push('/billing');
-        }else{
-            router.push('/dashboard/create-interview');
-        }
+    if (!user?.credits || user?.credits < 1) {
+      toast.error(
+        "You don't have enough credits! Purchase our plan to create interviews."
+      );
+      router.push("/billing");
+    } else {
+      router.push("/dashboard/create-interview");
     }
+  };
 
   const GetInterviewList = async () => {
     let { data: Interviews, error } = await supabase
@@ -34,28 +38,32 @@ function LatestInterviewsList() {
       .limit(6);
     // console.log(Interviews)
     setInterviewList(Interviews);
+    setLoading(false);
   };
 
   return (
-    <div className="my-5">
-      <h2 className="font-bold text-2xl">Previously Created Interviews</h2>
-
-      {interviewList?.length == 0 && (
-        <div className="p-5 flex flex-col gap-3 items-center mt-5">
-          <Video className="h-10 w-10 text-primary" />
-          <h2>You don't have any interview created!</h2>
-          <Button>Create New Interview</Button>
+        loading ? <PageLoader load={loading} /> :
+        <div className=' my-5'>
+            <h2 className=' font-bold text-2xl'>Previously Created Interviews</h2>
+            {
+                interviewList?.length==0 ?
+                <div className=' p-5 flex flex-col gap-3 items-center mt-5 bg-white rounded-xl'>
+                    <Video className=' h-10 w-10 text-primary' />
+                    <h2>You don't have any interview created!</h2>
+                    <div onClick={handleInterview}>
+                        <Button className=' cursor-pointer'><Plus/> Create New Interview</Button>                
+                    </div>
+                </div> :
+                <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+                    {
+                        interviewList.map((interview, index) => (
+                            <InterviewCard key={index} interview={interview} />
+                        ))
+                    }
+                </div>
+            }
         </div>
-      )}
-      {interviewList && (
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-5 gap-5">
-          {interviewList.map((interview, index) => (
-            <InterviewCard interview={interview} key={index} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    )
 }
 
 export default LatestInterviewsList;
